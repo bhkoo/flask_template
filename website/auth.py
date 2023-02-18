19a283563a1113eb296c6bada3defab5b2fbb5a2
Bonhwang Koo <bhkoo5@gmail.com>
2023-02-18 17:31:07 -0600

    Signup and login pages, message flash

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template('login.html', text='Testing', user = 'Gamby', boolean = True)
 
@@ -10,6 +10,22 @@ def login():
 def logout():
     return render_template('logout.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category = 'error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character', category = 'error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category = 'error')
+        elif len(password1) < 7:
+            flash('Passwords must be greater than 7 characters', category = 'error')
+        else:
+            flash('Accounts created', category = 'success')
     return render_template('signup.html')
